ac0fddaa7add2963ba5450465728b0d87684632e
Rahul-harkanchi <106588131+Rahul-harkanchi@users.noreply.github.com>
2025-12-21 20:34:46 +0530

    Day one of learning Python

---
+++ day1/string_manipulation.py
@@ -0,0 +1,5 @@
+#next line using \n
+print("Hello world \n Hello world ")
+#string manipulation
+print("hello"+"world")
+print("hello" + " "+ "world")
---
+++ day1/variables.py
@@ -0,0 +1,6 @@
+#variables are the way to save a perticular type of data 
+#example
+name = input("what is your name?")
+print("hello "+name)
+name_len = len(name)	
+print("your name has "+str(name_len)+"characters")
